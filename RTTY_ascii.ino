f9d8771bda8e0fb5b083805cf942a752b3ef7c28
slange1 <35535205+slange1@users.noreply.github.com>
2018-03-31 16:23:05 +0200

    Add files via upload

---
+++ RTTY_ascii.ino
@@ -0,0 +1,50 @@
+#define tx 2
+#define data 3
+#define t1 20
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(tx, OUTPUT);
+  pinMode(data,OUTPUT);
+  digitalWrite(data,HIGH);
+  delay(100);
+  Serial.println("Start");
+}
+
+void loop() {
+  if(Serial.available()>0){
+    digitalWrite(data,HIGH);
+    String text = Serial.readString();
+    int i;
+    for(i=0; text[i]!='\0'; i+=1);
+    Serial.println(i);
+    short t;
+    for(t=0; t<i; t+=1){
+    fsk(text[t]);
+    }
+  }
+}
+
+void fsk(char c){
+  tone(tx, 1275);
+  digitalWrite(data, LOW);
+  delay(t1);
+  int i;
+  for (i=0; i<=7; i+=1){
+    if((c>>i) & 1){
+        tone(tx, 2125);
+        digitalWrite(data,HIGH);
+        delay(t1);
+      }
+      else{
+        tone(tx, 1275);
+        digitalWrite(data,LOW);
+        delay(t1);
+     }  
+  }
+  tone(tx, 2125);
+  digitalWrite(data,HIGH);
+  delay(2*t1);
+  //digitalWrite(data,LOW);
+  noTone(tx);
+}
---
+++ Robot.ino
@@ -0,0 +1,46 @@
+int trig = 12;
+int echo = 11;
+int f1 = 2;
+int r1 = 3;
+int f2 = 4;
+int r2 = 5;
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(f1, OUTPUT);
+  pinMode(r1, OUTPUT);
+  pinMode(f2, OUTPUT);
+  pinMode(r2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(16);
+  digitalWrite(trig, LOW);
+  int dist = pulseIn(echo, HIGH)/58;
+  Serial.println(dist);
+  if(dist > 10){
+    forward();
+    delay(100);
+  }
+  else{
+    backward();
+    delay(1500);
+  }
+
+}
+void forward(){
+  analogWrite(r1, 0);
+  analogWrite(r2, 0);  
+  delay(10);
+  analogWrite(f1, 255);
+  analogWrite(f2, 255);
+}
+void backward(){
+  analogWrite(f1, 0);
+  analogWrite(f2, 0);  
+  delay(10);
+  analogWrite(r1, 255);
+  analogWrite(r2, 255);
+}
